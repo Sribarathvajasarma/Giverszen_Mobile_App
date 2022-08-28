f6de42fd49657c9e93de92200901cdf38da4daad
vithiyasahar <vithiyasaharvigneswaran@gmail.com>
2022-08-28 17:39:44 +0530

    change some thing in AuthStack.js

--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -56,6 +56,10 @@ import SplashScreen from '../screens/SplashScreen';
 import SignUp from '../screens/SignUp';
 import Login from '../screens/Login';
 import OnBoardingScreen from '../screens/OnBoardingScreen';
+import ForgotPassword from '../screens/ForgotPassword';
+import PinNumber from '../screens/PinNumber';
+import ResetPasswordScreen from '../screens/ResetPasswordScreen';
+
 const Stack = createNativeStackNavigator();
 const AuthStack = ({ navigation }) => {
     return (
@@ -63,6 +67,9 @@ const AuthStack = ({ navigation }) => {
             <Stack.Screen component={SplashScreen} name="Splash" />
             <Stack.Screen component={Login} name="Login" />
             <Stack.Screen component={SignUp} name="Signup" />
+            <Stack.Screen component={ForgotPassword} name="ForgotPassword" />
+            <Stack.Screen component={PinNumber} name="PinNumber" />
+            <Stack.Screen component={ResetPasswordScreen} name="ResetPasswordScreen" />
 
 
 
