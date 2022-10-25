ae4ca0a5d1f80715d28efc1c8e706e6085b4226a
Thuvethika <thuvethika@gmail.com>
2022-10-25 16:35:57 +0530

    non smart phone user update

--- src/screens/AddUser.js
+++ src/screens/AddUser.js
@@ -1,39 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 // import all the components we are going to use
-import { SafeAreaView, StyleSheet, View, Text, Button, TextInput, TouchableOpacity, Dimensions, ScrollView, } from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, Button, TextInput, TouchableOpacity, Dimensions, ScrollView, Alert } from 'react-native';
 import Checkbox from 'expo-checkbox';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { mapStyle } from '../global/mapStyle';
 import * as Location from 'expo-location'
+//import basic react native components
+
+
 
 const AddUser = ({ navigation }) => {
     const [visible, setVisible] = useState(false);
     const [isChecked, setChecked] = useState(false);
     //const [value, setValue] = useState(null);
     const [isFocus, setIsFocus] = useState(false);
-
-    const toggleBottomNavigationView = () => {
-        //Toggling the visibility state of the bottom sheet
-        setVisible(!visible);
+    const [coordinates, setCoordinates] = useState({ latitude: 9.704264, longitude: 80.069339 })
 
 
-    };
+
+    const map = useRef(1)
+    const [data, setData] = useState({
+        username: '',
+        phonenumber: '',
+        longitude: '',
+        latitude: '',
+    }
+
+    )
 
 
-   // const map = useRef(1)
 
+    const handleNameChange = (val) => {
+        setData({
+            ...data,
+            username: val,
+
+        })
+
+    }
+    const handlePhonenumberChange = (val) => {
+        setData({
+            ...data,
+            phonenumber: val,
+
+        })
+
+    }
+
+
+    // username,phonenumber,longitude,latitude,listingdistance
 
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
     const [latlng, setLatLng] = useState({})
     const [items, setItems] = useState([
-        { label: '1Km', value: '1Km' },
-        { label: '2Km', value: '2Km' },
+        { label: '1Km', value: '1' },
+        { label: '2Km', value: '2' },
 
     ])
+
 
     const button1handler = () => {
         checkPermission();
@@ -41,7 +69,90 @@ const AddUser = ({ navigation }) => {
         setCoordinates(latlng)
         console.log(latlng)
 
+    }
+    const getLocation = async () => {
+        try {
+            const { granted } = await Location.requestForegroundPermissionsAsync();
+            if (!granted) return;
+            const {
+                coords: { latitude, longitude },
+
+            } = await Location.getCurrentPositionAsync();
+            setLatLng({ latitude: latitude, longitude: longitude })
+        } catch (err) {
+
+        }
+    }
+
+    const checkPermission = async () => {
+        const hasPermission = await Location.requestForegroundPermissionsAsync();
+        if (hasPermission.status === 'granted') {
+            const permission = await askPermission()
+            return permission
+        }
+
+        return true
     };
+
+    const askPermission = async () => {
+        const permission = await Location.requestForegroundPermissionsAsync()
+        return permission.status === 'granted';
+    }
+
+
+    const handlesumit2 = () => {
+        console.log(data.username)
+        console.log(data.phonenumber)
+        console.log(data.longitude)
+        console.log(data.latitude)
+        console.log(value)
+
+
+
+        // if (username && phonenumber && longitude && latitude && listingdistance ) {
+        //  console.log("Correct data")
+
+    }
+
+    /*
+        if (username && phonenumber && longitude && latitude && listingdistance ) {
+            console.log("Correct data")
+            
+            fetch("https://giverzenbackend.herokuapp.com/api/nonsmartphoneuser_register", {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: id,
+                    username: username,
+                    phonenumber: phonenumber,
+                    longitude: longitude,
+                    latitude: latitude,
+                    listingdistance: listingdistance,
+                })
+            })
+    
+                .then((response) => response.json())
+                .then(async (responseData) => {
+                    console.log(responseData)
+                    if (responseData.code === 1) {
+                        Alert.alert('User added succesfully')
+    
+                    } else {
+                        Alert.alert('Sorry unable to add user, Please try again')
+    
+                    }
+    
+                })
+                .done();
+            return { listings: [...state.listings], isLoading: false }
+        } */
+    // const getVlaue = (val) => {
+    //     setValue(val)
+    // }
+
     return (
 
 
@@ -50,23 +161,30 @@ const AddUser = ({ navigation }) => {
                 <View style={styles.container}>
 
 
+
                     <Text style={{ margin: 20, fontSize: 15 }}>User Name</Text>
-                    <TextInput placeholder='Enter the user name'
+                    <TextInput placeholder='Enter the User Name'
                         style={{ borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 60, paddingLeft: 10, borderRadius: 10 }}
+                        onChangeText={(val) => handleNameChange(val)}
+                    />
+                    {/* {data.isValidName ? null :
+                            <Animatable.View animation="fadeInLeft" duration={500}>
+                                <Text style={styles.errorMsg}>Name can't be empty</Text>
+                            </Animatable.View>
+                        } */}
 
+
+                    <Text style={{ margin: 20, fontSize: 15 }}>Phone Number</Text>
+                    <TextInput placeholder='Enter the Phone Number'
+                        style={{ borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 60, paddingLeft: 10, borderRadius: 10 }}
+                        onChangeText={(val) => handlePhonenumberChange(val)}
                     />
 
-                    <Text style={{ margin: 20, fontSize: 15 }}> Phone Number</Text>
-                    <TextInput placeholder='Enter the phone number'
-                        style={{ borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 60, paddingLeft: 10, borderRadius: 10 }}
-
-                    />
                     <Text style={{ margin: 20, fontSize: 15 }}>Pickup Location</Text>
                     <View style={{ alignItems: "center", justifyContent: "center" }}>
-                    <MapView
+                        <MapView
                             provider={PROVIDER_GOOGLE}
-                           
-                         
+                            ref={map}
                             style={styles.map}
                             customMapStyle={mapStyle}
                             showsUserLocation={true}
@@ -87,7 +205,7 @@ const AddUser = ({ navigation }) => {
 
 
 
-                        </MapView> 
+                        </MapView>
                         <View style={{
 
                             position: 'absolute',//use absolute position to show button on top of the map
@@ -96,7 +214,7 @@ const AddUser = ({ navigation }) => {
                             width: Dimensions.get('window').width * 0.50,
                         }}
                         >
-                            <Button title="Locate me" />
+                            <Button title="Locate me" onPress={button1handler} />
                         </View>
                     </View>
 
@@ -109,14 +227,12 @@ const AddUser = ({ navigation }) => {
                         setOpen={setOpen}
                         setValue={setValue}
                         setItems={setItems}
-                        placeholder="Select distance"
 
                     />
                     <Text style={{ margin: 20, fontSize: 15 }}>
                         <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked} />  I accept to get notify when there is nearest food listings.</Text>
-
                     <View style={styles.button}>
-                        <TouchableOpacity style={styles.buttonContainer} >
+                        <TouchableOpacity style={styles.buttonContainer} onPress={handlesumit2}>
                             <Text style={styles.buttonText}>Save</Text>
                         </TouchableOpacity>
 
