ae33b058351ddc0f545b34631ded3da04d09a982
AntonSinthujan <antonsinthujan@gmail.com>
2022-10-26 16:48:26 +0530

    edit profile screen

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -300,124 +300,124 @@ const EditProfileScreen = ({ navigation }) => {
 
 export default EditProfileScreen;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    commandButton: {
-        padding: 15,
-        borderRadius: 10,
-        backgroundColor: '#FF6347',
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    panel: {
-        padding: 20,
-        backgroundColor: '#FFFFFF',
-        paddingTop: 20,
-        width: '100%',
-    },
-    header: {
-        backgroundColor: '#FFFFFF',
-        shadowColor: '#333333',
-        shadowOffset: { width: -1, height: -3 },
-        shadowRadius: 2,
-        shadowOpacity: 0.4,
-        paddingTop: 20,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-    },
-    panelHeader: {
-        alignItems: 'center',
-    },
-    panelHandle: {
-        width: 40,
-        height: 8,
-        borderRadius: 4,
-        backgroundColor: '#00000040',
-        marginBottom: 10,
-    },
-    panelTitle: {
-        fontSize: 27,
-        height: 35,
-    },
-    panelSubtitle: {
-        fontSize: 14,
-        color: 'gray',
-        height: 30,
-        marginBottom: 10,
-    },
-    panelButton: {
-        padding: 13,
-        borderRadius: 10,
-        backgroundColor: '#2e64e5',
-        alignItems: 'center',
-        marginVertical: 7,
-    },
-    panelButtonTitle: {
-        fontSize: 17,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    action: {
-        flexDirection: 'row',
-        marginTop: 10,
-        marginBottom: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#f2f2f2',
-        paddingBottom: 5,
-    },
-    actionError: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#FF0000',
-        paddingBottom: 5,
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -8,
-        paddingLeft: 10,
-        color: '#333333',
-    },
-    button: {
-        alignItems: 'center',
-        marginTop: 20
-    },
-    signIn: {
-        width: '80%',
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10
-    },
-    textSign: {
-        fontSize: 18,
-        fontWeight: 'bold'
-    },
-    buttonContainer: {
-        marginTop: 10,
-        height: Dimensions.get('window').height / 10,
-        backgroundColor: '#009387',
-        padding: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 10,
-        width: Dimensions.get('window').width * 0.8,
-    },
-    buttonText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#ffffff',
-    },
-    uploadBtn: {
-        height: 125,
-        width: 125,
-        borderRadius: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//     },
+//     commandButton: {
+//         padding: 15,
+//         borderRadius: 10,
+//         backgroundColor: '#FF6347',
+//         alignItems: 'center',
+//         marginTop: 10,
+//     },
+//     panel: {
+//         padding: 20,
+//         backgroundColor: '#FFFFFF',
+//         paddingTop: 20,
+//         width: '100%',
+//     },
+//     header: {
+//         backgroundColor: '#FFFFFF',
+//         shadowColor: '#333333',
+//         shadowOffset: { width: -1, height: -3 },
+//         shadowRadius: 2,
+//         shadowOpacity: 0.4,
+//         paddingTop: 20,
+//         borderTopLeftRadius: 20,
+//         borderTopRightRadius: 20,
+//     },
+//     panelHeader: {
+//         alignItems: 'center',
+//     },
+//     panelHandle: {
+//         width: 40,
+//         height: 8,
+//         borderRadius: 4,
+//         backgroundColor: '#00000040',
+//         marginBottom: 10,
+//     },
+//     panelTitle: {
+//         fontSize: 27,
+//         height: 35,
+//     },
+//     panelSubtitle: {
+//         fontSize: 14,
+//         color: 'gray',
+//         height: 30,
+//         marginBottom: 10,
+//     },
+//     panelButton: {
+//         padding: 13,
+//         borderRadius: 10,
+//         backgroundColor: '#2e64e5',
+//         alignItems: 'center',
+//         marginVertical: 7,
+//     },
+//     panelButtonTitle: {
+//         fontSize: 17,
+//         fontWeight: 'bold',
+//         color: 'white',
+//     },
+//     action: {
+//         flexDirection: 'row',
+//         marginTop: 10,
+//         marginBottom: 10,
+//         borderBottomWidth: 1,
+//         borderBottomColor: '#f2f2f2',
+//         paddingBottom: 5,
+//     },
+//     actionError: {
+//         flexDirection: 'row',
+//         marginTop: 10,
+//         borderBottomWidth: 1,
+//         borderBottomColor: '#FF0000',
+//         paddingBottom: 5,
+//     },
+//     textInput: {
+//         flex: 1,
+//         marginTop: Platform.OS === 'ios' ? 0 : -8,
+//         paddingLeft: 10,
+//         color: '#333333',
+//     },
+//     button: {
+//         alignItems: 'center',
+//         marginTop: 20
+//     },
+//     signIn: {
+//         width: '80%',
+//         height: 50,
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         borderRadius: 10
+//     },
+//     textSign: {
+//         fontSize: 18,
+//         fontWeight: 'bold'
+//     },
+//     buttonContainer: {
+//         marginTop: 10,
+//         height: Dimensions.get('window').height / 10,
+//         backgroundColor: '#009387',
+//         padding: 10,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         borderRadius: 10,
+//         width: Dimensions.get('window').width * 0.8,
+//     },
+//     buttonText: {
+//         fontSize: 18,
+//         fontWeight: 'bold',
+//         color: '#ffffff',
+//     },
+//     uploadBtn: {
+//         height: 125,
+//         width: 125,
+//         borderRadius: 10,
+//         alignItems: 'center',
+//         justifyContent: 'center',
 
 
-    },
-});
+//     },
+// });
