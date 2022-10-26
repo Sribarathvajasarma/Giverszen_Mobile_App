268912596c17f33ceea74491646467a40c5b1541
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2022-10-26 22:30:07 +0530

    Modifying map screen

--- src/screens/Maps.js
+++ src/screens/Maps.js
@@ -1,58 +1,27 @@
-
-
-
-
 import React, { Component, useState, useEffect } from 'react';
-import {
-    StyleSheet,
-    Button,
-    Text,
-    View,
-    TextInput,
-    ImageBackground,
-    ActivityIndicator,
-    Linking,
-    TouchableOpacity,
-    LogBox,
-    Image
-} from 'react-native';
+import { StyleSheet, Button, Text, View, ScrollView, TouchableOpacity, LogBox, Image } from 'react-native';
 import styled from 'styled-components/native';
-import PropTypes from 'prop-types';
 import MapView from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
 import getDirections from 'react-native-google-maps-directions'
+import { GOOGLE_MAPS_APIKEY } from "@env"
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-
-
-
-export default function MapFunction({ route }) {
+export default function MapFunction({ navigation, route }) {
     const [seconds, setSeconds] = useState(0);
     const [cords, setCords] = useState({})
     const [region, setRegion] = useState({})
     const itemData = route.params.itemData
     const location = route.params.location
 
+    useEffect(() => {
+        LogBox.ignoreLogs(['Failed prop type']);                       //Avoid warnings
 
-    // useEffect(() => {
-    //     LogBox.ignoreLogs(['Error on GMAPS route request: ZERO_RESULTS']);
-    // }, [])
-
-    // const fetch_data = async () => {
-    //     let response = await fetch("https://giverzen6.herokuapp.com/api/get")
-    //     if (!response.ok) {
-    //         throw new Error(`Error! status: ${response.status}`);
-    //     }
-    //     response = await response.json()
-    //     setCords({ latitude: parseFloat(response.results[0].latitude), longitude: parseFloat(response.results[0].longitude) })
-    // }
-
-
-
-
+    }, [])
 
     useEffect(() => {
-
-        const find_region = () => {
+        const find_region = () => {                                     //Function to find appropriate initial region for the given points
             const regionContainingPoints = points => {
                 let minLat, maxLat, minLng, maxLng;
 
@@ -80,7 +49,7 @@ export default function MapFunction({ route }) {
 
                 return {
                     latitude: midLat, longitude: midLng,
-                    latitudeDelta: deltaLat + 0.05, longitudeDelta: deltaLng + 0.05,
+                    latitudeDelta: deltaLat + 0.001, longitudeDelta: deltaLng + 0.001,
                 };
             }
 
@@ -88,37 +57,151 @@ export default function MapFunction({ route }) {
             const data = regionContainingPoints([
                 location, itemData.coordinate
             ]);
+
             setRegion(data)
         }
-
         find_region()
     }, [location, itemData])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     if (itemData && location && region) {
         return (
-
-            <MapClass hookValue={itemData.coordinate} Location={location} region={region}></MapClass>
-
+            <View style={styles.container}>
+                <ScrollView>
+                    <View style={[styles.section, { height: 500 }]}>
+                        <MapClass hookValue={itemData.coordinate} Location={location} region={region}></MapClass>
+                    </View>
+                    <View style={styles.section}>
+                        <Text style={styles.title}>vfvdvdvdf</Text>
+                    </View>
+                    <View style={[styles.section, { height: 100 }]}>
+                        <Text style={styles.title}>Best Before</Text>
+                        <Text style={[styles.sectionContent, { color: '#009387' }]}>22 Aug 2022 10.00.00</Text>
+                    </View>
+                    <View style={[styles.section, { height: 100 }]}>
+                        <Text style={styles.title}>Poster Name</Text>
+                        <Text style={styles.sectionContent}>Vithiyasahar</Text>
+                    </View>
+                    <View style={[styles.section, { height: 150 }]}>
+                        <Text style={styles.title}>description</Text>
+                        <Text style={styles.sectionContent}>dvdfvdfvd</Text>
+                    </View>
+                    <View style={styles.section}>
+                        <Text style={styles.title}>Action</Text>
+                        <View style={styles.categories}>
+                            <TouchableOpacity>
+                                <View style={styles.categoryContainer}>
+                                    <Entypo name="message" size={20} style={{ marginTop: 1 }} color="#fff" />
+                                    <Text style={styles.category}>Chat</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => navigation.navigate('driver', { item: itemData, location: location })}>
+                                <View style={styles.categoryContainer}>
+                                    <AntDesign name="car" size={20} style={{ marginTop: 1 }} color="#fff" />
+                                    <Text style={styles.category}>Request a driver</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ScrollView>
+            </View>
         );
     }
-
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+    },
+    name: {
+        fontWeight: 'bold',
+    },
+    section: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
+        backgroundColor: 'white',
+    },
+    sectionTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    sectionContent: {
+        fontSize: 16,
+        textAlign: 'justify',
+    },
+    categories: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexWrap: 'wrap',
+    },
+    categoryContainer: {
+        flexDirection: 'row',
+        backgroundColor: '#009387',
+        borderRadius: 20,
+        margin: 10,
+        padding: 10,
+        paddingHorizontal: 15,
+    },
+    category: {
+        fontSize: 14,
+        color: '#fff',
+        marginLeft: 10,
+    },
+    titleContainer: {
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageTitle: {
+        color: 'white',
+        backgroundColor: 'transparent',
+        fontSize: 24,
+    },
+    navTitle: {
+        color: 'white',
+        fontSize: 18,
+        backgroundColor: 'transparent',
+    },
+    sectionLarge: {
+        minHeight: 300,
+    },
+    mainCardView: {
+        height: 90,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "#fff",
+        borderRadius: 15,
+        shadowColor: "#050505",
+        shadowOffset: { width: 0, height: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 8,
+        elevation: 8,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 16,
+        paddingRight: 14,
+        marginTop: 6,
+        marginBottom: 6,
+        marginLeft: 16,
+        marginRight: 16,
+    },
+    subCardView: {
+        height: 50,
+        width: 50,
+        borderRadius: 25,
+        backgroundColor: "#FC0808 ",
+        borderColor: "#050505",
+        borderWidth: 1,
+        borderStyle: 'solid',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
 
 let Container = styled(View)`
 	width: 100%;
@@ -131,25 +214,15 @@ let Map = styled(MapView)`
 	height: 100%;
 `;
 
-
-
-// const origin = { latitude: 9.704264, longitude: 80.069339 };
-
-// const destination = { latitude: 9.712623, longitude: 80.058015 };
-
-const GOOGLE_MAPS_APIKEY = 'AIzaSyBg9yp_6i0LuulJmzGrWFyVvUJaL7pwef4';
-
 class MapClass extends Component {
 
     constructor() {
-
         super();
         this.state = {
             distance: 0,
             duration: 0
         };
     }
-
 
     handleGetDirections = () => {
         const data = {
@@ -161,13 +234,11 @@ class MapClass extends Component {
                 }
             ]
         }
-
         getDirections(data)
     }
 
     pressed(e) {
         console.log('pressed');
-
     }
 
     render() {
@@ -176,29 +247,18 @@ class MapClass extends Component {
 
         return (
             <Container>
-                <Map
-                    initialRegion={this.props.region}
-                >
+                <Map initialRegion={this.props.region}>
                     <MapView.Marker
-                        coordinate={someHookValue}
-                    >
+                        coordinate={someHookValue}>
                         <MapView.Callout onPress={() => {
                             console.log('Press to Get Direction');
                             this.handleGetDirections();
                         }}>
                             <Text>Press to Get Direction</Text>
-
                         </MapView.Callout>
                     </MapView.Marker>
-                    {current && <MapView.Marker
-                        coordinate={current}
-
-                    ><Image source={require('../../assets/human.png')} style={{ height: 50, width: 50 }} />
-
+                    {current && <MapView.Marker coordinate={current}><Image source={require('../../assets/human.png')} style={{ height: 50, width: 50 }} />
                     </MapView.Marker>}
-                    {/* <MapView.Marker coordinate={{ latitude: 80.0000, longitude: 7.0000 }}><Image source={require('../../assets/human.png')} style={{ height: 35, width: 35 }} /></MapView.Marker> */}
-
-
                     <MapViewDirections
                         origin={current}
                         destination={someHookValue}
@@ -206,36 +266,13 @@ class MapClass extends Component {
                         strokeWidth={3}
                         strokeColor="blue"
                         onReady={result => {
-
                             this.setState({ distance: result.distance, duration: Number((result.duration).toFixed(2)) })
-
                             this.forceUpdate()
-                        }}
-
-                    />
-
+                        }} />
                 </Map>
-
-                <View style={{
-                    position: 'absolute',
-                    margin: 20,
-                    right: 0,
-                    bottom: 10,
-                    height: 100,
-                    width: 200,
-                    backgroundColor: '#DEF7FA',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    shadowColor: '#171717',
-                    shadowOffset: { width: -2, height: 4 },
-                    shadowOpacity: 0.2,
-                    shadowRadius: 3,
-
-                }}>
+                <View style={{ position: 'absolute', margin: 20, right: 0, bottom: 10, height: 100, width: 200, backgroundColor: '#DEF7FA', alignItems: 'center', justifyContent: 'center', shadowColor: '#171717', shadowOffset: { width: -2, height: 4 }, shadowOpacity: 0.2, shadowRadius: 3, }}>
                     <Text>distance : {this.state.distance} km</Text>
                     <Text>time : {this.state.duration} min</Text></View>
-
-
             </Container>
         );
     }
