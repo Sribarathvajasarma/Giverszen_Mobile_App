9bd07bb9d6043fde54391d449ecbd0c0480ef849
Thuvethika <thuvethika@gmail.com>
2022-10-26 18:14:36 +0530

    add forum function

--- src/screens/Addforum.js
+++ src/screens/Addforum.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Platform, StatusBar, TextInput, TouchableOpacity, Dimensions, Button, Image, ScrollView, LogBox, SafeAreaView, Alert } from 'react-native'
+import { StyleSheet, Text, View, Platform, StatusBar, TextInput, TouchableOpacity, Dimensions, Button, Image, ScrollView, LogBox, SafeAreaView,Alert } from 'react-native'
 import React, { useState, useEffect, useRef, useContext } from 'react'
 
 import * as ImagePicker from 'expo-image-picker';
@@ -17,7 +17,7 @@ import { Avatar } from 'react-native-paper';
 
 
 
-function AddForum() {
+function Addforum() {
     const [image, setImage] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -26,12 +26,12 @@ function AddForum() {
 
 
     const [data, setData] = useState({
-
-        category: "",
+       
+        category:"",
         description: "",
-        image: ""
-
-
+        image:""
+       
+        
 
 
 
@@ -74,7 +74,7 @@ function AddForum() {
     }
 
 
-
+    
 
 
 
@@ -102,122 +102,122 @@ function AddForum() {
             category: val
         })
     }
+  
+    
 
+   const  handleSubmit = async() => {
 
+    
 
-    const handleSubmit = async () => {
-
-
-
-        let userId = await AsyncStorage.getItem('userId')
-        let userAvatar = await AsyncStorage.getItem('userAvatar')
-        // const userId = await AsyncStorage.getItem('userId', userId)
-        // const avatarId = await AsyncStorage.getItem('userAvatar',avatarId)
-
-
-        console.log(data.category)
-        console.log(data.description)
-        console.log(data.image)
-        console.log(userId)
-
-
-
-        fetch("https://giverzenbackend.herokuapp.com/api/posts", {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                category: data.category,
-                Description: data.description,
-                postImage: data.image,
-                userID: userId,
-
-
-            })
+     let userId = await AsyncStorage.getItem('userId')
+     let userAvatar = await AsyncStorage.getItem('userAvatar')
+   // const userId = await AsyncStorage.getItem('userId', userId)
+   // const avatarId = await AsyncStorage.getItem('userAvatar',avatarId)
+    
+    
+    console.log(data.category)
+    console.log(data.description)
+    console.log(data.image)
+    console.log(userId)
+    
+    
+    
+    fetch("https://giverzenbackend.herokuapp.com/api/posts", {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+            category:data.category,
+            Description:data.description,
+            postImage:data.image,
+            userID: userId,
+         
+          
         })
+    })
 
-            .then((response) => response.json())
-            .then(async (responseData) => {
-                console.log(responseData)
-                if (responseData.code === 1) {
-                    Alert.alert('Forum added succesfully')
+        .then((response) => response.json())
+        .then(async (responseData) => {
+            console.log(responseData)
+            if (responseData.code === 1) {
+                Alert.alert('Forum added succesfully')
 
-                } else {
-                    Alert.alert('Sorry unable to add Forum, Please try again')
+            } else {
+                Alert.alert('Sorry unable to add Forum, Please try again')
 
-                }
+            }
 
-            })
-            .done();
+        })
+        .done();
 
-    }
-
-    return (
-        <SafeAreaView style={styles.container}>
+   }
+  
+   return (
+    <SafeAreaView style={styles.container}>
             <ScrollView style={{ marginBottom: 20 }}>
                 <View>
+               
+    
+
+                <View style={{ flexDirection: 'row', marginTop: 50, marginLeft: 30, marginBottom: 20 }}>
+                            <TouchableOpacity style={[styles.uploadBtn, !image ? {
+                                borderStyle: 'dashed', borderWidth: 1
+                            } : { borderStyle: 'none' }]} onPress={pickImage}>
+                                {image && <Image source={{ uri: image }} style={styles.image} />}
+                                {!data.avatar && <Text style={{ textAlign: 'center', fontSize: 16, opacity: 0.3, fontWeight: 'bold' }}>Upload Image</Text>}
+
+
+                            </TouchableOpacity>
+
+                        </View>
+                    
+    <Text style={{ margin: 20, fontSize: 15 }}>Category</Text>
+    <TextInput placeholder='Enter the food type'
+                            style={{ borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 60, paddingLeft: 10, borderRadius: 10 }}
+                            onChangeText={(val) => handleCategoryChange(val)}
+                        />   
+    <Text style={{ margin: 20, fontSize: 15 }}>Description</Text>
+
+     <TextInput
+                            style={{ textAlignVertical: 'top', borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 100, paddingLeft: 10, borderRadius: 10 }}
+                            numberOfLines={5}
+                            placeholder="Description"
+                            multiline
+                            onChangeText={(val) => handleDescriptionChange(val)}
+                        />
 
 
 
-                    <View style={{ flexDirection: 'row', marginTop: 50, marginLeft: 30, marginBottom: 20 }}>
-                        <TouchableOpacity style={[styles.uploadBtn, !image ? {
-                            borderStyle: 'dashed', borderWidth: 1
-                        } : { borderStyle: 'none' }]} onPress={pickImage}>
-                            {image && <Image source={{ uri: image }} style={styles.image} />}
-                            {!data.avatar && <Text style={{ textAlign: 'center', fontSize: 16, opacity: 0.3, fontWeight: 'bold' }}>Upload Image</Text>}
+                        <View style={styles.button}>
+                            <TouchableOpacity style={styles.buttonContainer} onPress={handleSubmit}>
+                                <Text style={styles.buttonText}>Add Forum</Text>
+                            </TouchableOpacity>
 
+                            <TouchableOpacity onPress={() => navigation.navigate('Home')}
+                                style={[styles.signIn, {
+                                    borderColor: '#009387',
+                                    borderWidth: 1,
+                                    marginTop: 15,
+                                    width: '90%'
 
-                        </TouchableOpacity>
-
-                    </View>
-
-                    <Text style={{ margin: 20, fontSize: 15 }}>Category</Text>
-                    <TextInput placeholder='Enter the food type'
-                        style={{ borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 60, paddingLeft: 10, borderRadius: 10 }}
-                        onChangeText={(val) => handleCategoryChange(val)}
-                    />
-                    <Text style={{ margin: 20, fontSize: 15 }}>Description</Text>
-
-                    <TextInput
-                        style={{ textAlignVertical: 'top', borderWidth: 1, borderColor: '#000000', marginHorizontal: 20, height: 100, paddingLeft: 10, borderRadius: 10 }}
-                        numberOfLines={5}
-                        placeholder="Description"
-                        multiline
-                        onChangeText={(val) => handleDescriptionChange(val)}
-                    />
-
-
-
-                    <View style={styles.button}>
-                        <TouchableOpacity style={styles.buttonContainer} onPress={handleSubmit}>
-                            <Text style={styles.buttonText}>Add Forum</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity onPress={() => navigation.navigate('Home')}
-                            style={[styles.signIn, {
-                                borderColor: '#009387',
-                                borderWidth: 1,
-                                marginTop: 15,
-                                width: '90%'
-
-                            }]}
-                        >
-                            <Text style={[styles.textSign, {
-                                color: '#009387'
-                            }]}>Cancel</Text>
-                        </TouchableOpacity>
-                    </View>
-
-
-
-                </View>
-            </ScrollView>
+                                }]}
+                            >
+                                <Text style={[styles.textSign, {
+                                    color: '#009387'
+                                }]}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+  
+  
+             
+             </View>
+          </ScrollView>
         </SafeAreaView>
-
-
-    )
+                        
+                      
+  )
 }
 
 export default Addforum
